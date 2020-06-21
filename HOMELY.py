fa1f0745ee07dd6e25da11734301c9f2f8b7803e
Fekete Krisztián <e3krisztian@gmail.com>
2020-06-21 15:40:41 +0200

    remove installpkg - does not seem to work with pacman

--- HOMELY.py
+++ HOMELY.py
@@ -1,5 +1,5 @@
 from homely.files import mkdir, symlink
-from homely.install import installpkg
+# from homely.install import installpkg
 
 mkdir("~/.config")
 
@@ -12,16 +12,16 @@ symlink(".profile", "~/.bash_profile")
 symlink(".bashrc")
 symlink(".bashrc.d")
 
-installpkg("i3-wm")
-installpkg("i3lock")
+# installpkg("i3-wm")
+# installpkg("i3lock")
 symlink(".config/i3", "~/.config/i3")
 
-installpkg("i3status")
+# installpkg("i3status")
 symlink(".config/i3status", "~/.config/i3status")
 
-installpkg("keynav")
+# installpkg("keynav")
 symlink(".keynavrc")
 
 symlink(".inputrc")
-installpkg("tmux")
+# installpkg("tmux")
 symlink(".tmux.conf")
