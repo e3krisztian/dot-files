da8fa88c786328318c6d842f46a6db0a5866e761
Fekete Krisztián <e3krisztian@gmail.com>
2024-07-15 23:21:01 +0200

    PYTHONSTARTUP: less moving bits + self-documenting help message

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -26,36 +26,58 @@ readline.read_init_file()
 # else:
 #     readline.write_history_file(".python_history")
 
-class _PathString:
-    def __truediv__(self, other):
-        return Path(str(other))
-
-P = p = _PathString()
-del _PathString
-
-
-class _PPrint:
-    def __truediv__(self, other):
-        pprint(other)
-
-x = _PPrint()
-del _PPrint
+def _slash_command(f):
+    class SlashCommand:
+        def __truediv__(self, data):
+            return f(data)
+        def __repr__(self):
+            return f.__doc__
+    SlashCommand.__name__ = f.__name__
+    SlashCommand.__doc__ = f.__doc__
+    return SlashCommand()
 
 
-class _Print:
-    def __truediv__(self, other):
-        print(other)
-
-X = _Print()
-del _Print
+@_slash_command
+def p(path):
+    """p/"path" = Path("path")"""
+    return Path(str(path))
 
 
-class _Help:
-    def __truediv__(self, other):
-        help(other)
+@_slash_command
+def l(iterable):
+    """l/iterable = list(iterable)"""
+    return list(iterable)
 
-h = _Help()
-del _Help
+
+@_slash_command
+def h(object):
+    """h/object = help(object)"""
+    help(object)
+
+
+@_slash_command
+def x(object):
+    """x/object = pprint(object)"""
+    width = int(os.environ.get("COLUMNS", 80))
+    pprint(object, width=width, compact=True)
+
+
+@_slash_command
+def xl(iterable):
+    """xl/iterable = pprint(list(iterable))"""
+    pprint(list(iterable))
+
+
+@_slash_command
+def X(object):
+    """X/object = print(object)"""
+    print(object)
+
+
+@_slash_command
+def h(object):
+    """h/object = help(object)"""
+    help(object)
 
 
 def _highlight(code):
@@ -70,7 +92,6 @@ def _highlight(code):
     formatter = pygments.formatters.get_formatter_by_name("console")
     return pygments.highlight(code, lexer, formatter)
 
-
 try:
     import colorama
     _EMPH = colorama.Style.BRIGHT
@@ -78,30 +99,31 @@ try:
     _RESET= colorama.Back.RESET + colorama.Fore.RESET + colorama.Style.RESET_ALL
 except ImportError:
     _EMPH = ""
-    _RED = ""
+    _WARN = ""
     _RESET = ""
 
-class _Source:
-    def __truediv__(self, other):
-        try:
-            source = inspect.getsource(other)
-        except Exception:
-            print(f"Haiya {_WARN}\N{Slightly Frowning Face}{_RESET}! Could not get source for {_EMPH}{other}{_RESET} for you - so sad!")
-            return
 
-        # print(_highlight(source))
-        run(["/usr/bin/less", "-RF"], input=_highlight(source).encode())
-
-s = _Source()
-del _Source
-
-# PYTHONINSPECT & PYTHONSTARTUP:
-# https://gist.github.com/gwparikh/797559acf7d0164cfd32
+_SAD_FACE = f"{_WARN}\N{Slightly Frowning Face}{_RESET}"
 
 
-import code
-filename="demo.py"
-__file__ = filename
-c = code.InteractiveConsole(locals=locals(), filename=filename)
-c.runsource(Path(filename).read_text(), filename=filename, symbol="exec")
-c.interact()
+@_slash_command
+def s(object):
+    """s/object = print the Python source for object"""
+    try:
+        source = inspect.getsource(object)
+    except Exception:
+        print(f"Haiya {_SAD_FACE}! Could not get source of {_EMPH}{object}{_RESET} for you - so sad!")
+        return
+
+    # print(_highlight(source))
+    run(["/usr/bin/less", "-RF"], input=_highlight(source).encode())
+
+
+# WIP: interactive console with goodies after loading a module
+# # PYTHONINSPECT & PYTHONSTARTUP does not work together :(
+# import code
+# filename="demo.py"
+# __file__ = filename
+# c = code.InteractiveConsole(locals=locals(), filename=filename)
+# c.runsource(Path(filename).read_text(), filename=filename, symbol="exec")
+# c.interact()
